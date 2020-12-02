0631e64cba6915a3cb642af1892fac101adf2123
akutimi <62243479+akutimi@users.noreply.github.com>
2020-12-02 12:31:24 +0200

    Create testCmmdc.py

---
+++ teste/testCmmdc.py
@@ -0,0 +1,10 @@
+  
+from cmmdc import calcul
+
+
+def testCmmdc()
+  assert calcul(10,0)==0
+  assert calcul(10,5)==5
+  assert calcul(5,7)==1
+  assert calcul(12,18)==6
+  assert calcul(39,26)==13
